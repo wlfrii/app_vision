0a4ba6f99ed9fed7d5f156a1087f793d68b30216
wlfrii <longfei.wang@sjtu.edu.cn>
2021-10-15 22:43:06 +0800

    completed the gpu::display test

--- gpu/test/main.cpp
+++ gpu/test/main.cpp
@@ -4,24 +4,23 @@
 
 int main()
 {
-    std::string path = "../data/WechatIMG13.png";
-    std::string path2 = "../data/WechatIMG14.png";
-    cv::Mat image = cv::imread(path);
-    cv::Mat image2 = cv::imread(path2);
-    printf("Image: w %d,h %d,c %d, continuous %d.\n", image.cols, image.rows, image.channels(), image.isContinuous());
-
+    std::string l_path = "./L_test.bmp";
+    std::string r_path = "./R_test.bmp";
+    cv::Mat l_image = cv::imread(l_path);
+    cv::Mat r_image = cv::imread(r_path);
+    printf("Image: w %d,h %d,c %d, continuous %d.\n", l_image.cols, l_image.rows, l_image.channels(), l_image.isContinuous());
 
     gpu::Displayer displayer_3d(960, 540);
     displayer_3d.initialize(); 
     cv::Mat tmp;
-    cv::cvtColor(image,tmp,cv::COLOR_BGR2RGB);
-    displayer_3d.updateImage(tmp.ptr<uchar>(0), image.cols, image.rows, image.channels(), false);
-    cv::cvtColor(image2,tmp,cv::COLOR_BGR2RGB);
-    displayer_3d.updateImage(tmp.ptr<uchar>(0), image.cols, image.rows, image.channels(), true);
+    cv::cvtColor(l_image,tmp,cv::COLOR_BGR2RGB);
+    displayer_3d.updateImage(tmp.ptr<uchar>(0), tmp.cols, tmp.rows, tmp.channels(), false);
+    cv::cvtColor(r_image,tmp,cv::COLOR_BGR2RGB);
+    displayer_3d.updateImage(tmp.ptr<uchar>(0), tmp.cols, tmp.rows, tmp.channels(), true);
     
     gpu::Displayer displayer_2d(960, 540, gpu::Displayer::DISPLAY_2D);
     displayer_2d.initialize(); 
-    displayer_2d.updateImage(tmp.ptr<uchar>(0), image.cols, image.rows, image.channels());
+    displayer_2d.updateImage(tmp.ptr<uchar>(0), tmp.cols, tmp.rows, tmp.channels());
 
     while(!displayer_3d.shouldClose() && !displayer_2d.shouldClose())
     {
